1985be66591f1757f09869a82d5e4224d4364241
Sagid, home <kofon95@mail.ru>
2017-08-22 18:56:18 +0300

    add Thanks::union_publications_count

--- app/models/thanks.rb
+++ app/models/thanks.rb
@@ -1,4 +1,25 @@
 class Thanks < ApplicationRecord
   scope :published_thanks, -> { where(published: true) }
   # scope :unpublished_thanks, -> { where(published: false) }
+
+
+  # optimized count of publish and unpubliched
+  PUBLICATIONS_COUNT_SQL = <<SQL
+select cnt from (
+  select 0, count(*) cnt from (#{Thanks.where(published: false).to_sql})
+  union
+  select 1, count(*) cnt from (#{Thanks.where(published: true).to_sql})
+  order by 1
+)
+SQL
+  private_constant :PUBLICATIONS_COUNT_SQL
+
+  def self.union_publications_count
+    query_result = ActiveRecord::Base.connection.execute(PUBLICATIONS_COUNT_SQL)
+
+    {
+      unpublished: query_result[0][0],
+      publiched:   query_result[1][0]
+    }
+  end
 end
